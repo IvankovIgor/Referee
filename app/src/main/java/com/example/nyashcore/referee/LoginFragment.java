ca79ac9c2a559953dd840264f4590848da707433
Игорь Иваньков <ivankov.i.f@gmail.com>
2016-10-18 12:29:06 +0300

    Login via VK.

---
+++ app/src/main/java/com/example/nyashcore/referee/LoginFragment.java
@@ -0,0 +1,144 @@
+package com.example.nyashcore.referee;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.gorbin.asne.core.SocialNetwork;
+import com.github.gorbin.asne.core.SocialNetworkManager;
+import com.github.gorbin.asne.core.listener.OnLoginCompleteListener;
+import com.github.gorbin.asne.vk.VkSocialNetwork;
+import com.vk.sdk.VKScope;
+
+
+import java.util.List;
+
+public class LoginFragment extends Fragment implements SocialNetworkManager.OnInitializationCompleteListener, OnLoginCompleteListener {
+    public static SocialNetworkManager mSocialNetworkManager;
+    /**
+     * SocialNetwork Ids in ASNE:
+     * 1 - Twitter
+     * 2 - LinkedIn
+     * 3 - Google Plus
+     * 4 - Facebook
+     * 5 - Vkontakte
+     * 6 - Odnoklassniki
+     * 7 - Instagram
+     */
+    private Button vk;
+    private Button ok;
+
+    public LoginFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        ((LoginActivity)getActivity()).getSupportActionBar().setTitle(R.string.app_name);
+        // init buttons and set Listener
+        vk = (Button) rootView.findViewById(R.id.vk);
+        vk.setOnClickListener(loginClick);
+
+        //Get Keys for initiate SocialNetworks
+        String VK_KEY = getActivity().getString(R.string.vk_app_id);
+
+        //Chose permissions
+        String[] vkScope = new String[] {
+                VKScope.FRIENDS,
+                VKScope.WALL,
+                VKScope.PHOTOS,
+                VKScope.NOHTTPS,
+                VKScope.STATUS,
+        };
+
+        //Use manager to manage SocialNetworks
+        mSocialNetworkManager = (SocialNetworkManager) getFragmentManager().findFragmentByTag(LoginActivity.SOCIAL_NETWORK_TAG);
+
+        //Check if manager exist
+        if (mSocialNetworkManager == null) {
+            mSocialNetworkManager = new SocialNetworkManager();
+
+            //Init and add to manager VkSocialNetwork
+            VkSocialNetwork vkNetwork = new VkSocialNetwork(this, VK_KEY, vkScope);
+            mSocialNetworkManager.addSocialNetwork(vkNetwork);
+
+            //Initiate every network from mSocialNetworkManager
+            getFragmentManager().beginTransaction().add(mSocialNetworkManager, LoginActivity.SOCIAL_NETWORK_TAG).commit();
+            mSocialNetworkManager.setOnInitializationCompleteListener(this);
+        } else {
+            //if manager exist - get and setup login only for initialized SocialNetworks
+            if(!mSocialNetworkManager.getInitializedSocialNetworks().isEmpty()) {
+                List<SocialNetwork> socialNetworks = mSocialNetworkManager.getInitializedSocialNetworks();
+                for (SocialNetwork socialNetwork : socialNetworks) {
+                    socialNetwork.setOnLoginCompleteListener(this);
+                    initSocialNetwork(socialNetwork);
+                }
+            }
+        }
+        return rootView;
+    }
+
+    private void initSocialNetwork(SocialNetwork socialNetwork){
+        if(socialNetwork.isConnected()){
+            vk.setText("Show match list");
+        }
+    }
+    @Override
+    public void onSocialNetworkManagerInitialized() {
+        //when init SocialNetworks - get and setup login only for initialized SocialNetworks
+        for (SocialNetwork socialNetwork : mSocialNetworkManager.getInitializedSocialNetworks()) {
+            socialNetwork.setOnLoginCompleteListener(this);
+            initSocialNetwork(socialNetwork);
+        }
+    }
+
+    //Login listener
+
+    private View.OnClickListener loginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int networkId = 0;
+            networkId = VkSocialNetwork.ID;
+            SocialNetwork socialNetwork = mSocialNetworkManager.getSocialNetwork(networkId);
+            if(!socialNetwork.isConnected()) {
+                if(networkId != 0) {
+                    socialNetwork.requestLogin();
+                    LoginActivity.showProgress("Loading social person");
+                } else {
+                    Toast.makeText(getActivity(), "Wrong networkId", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                startProfile(socialNetwork.getID());
+            }
+        }
+    };
+
+    @Override
+    public void onLoginSuccess(int networkId) {
+        LoginActivity.hideProgress();
+//        startProfile(networkId);
+    }
+
+    @Override
+    public void onError(int networkId, String requestID, String errorMessage, Object data) {
+        LoginActivity.hideProgress();
+        Toast.makeText(getActivity(), "ERROR: " + errorMessage, Toast.LENGTH_LONG).show();
+    }
+
+    private void startProfile(int networkId){
+        Intent intent = new Intent(getActivity(), MatchListActivity.class);
+        startActivity(intent);
+//        ProfileFragment profile = ProfileFragment.newInstannce(networkId);
+//        getActivity().getSupportFragmentManager().beginTransaction()
+//                .addToBackStack("profile")
+//                .replace(R.id.container, profile)
+//                .commit();
+////                .commitAllowingStateLoss();
+    }
+}
